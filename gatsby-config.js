0e9dbb2191e3b19e6066a9c4127f7ea48de94833
Nikema Prophet <nikema.prophet@popschools.com>
2019-09-09 23:59:00 -0700

    add webmention plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,54 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
+  pathPrefix: "/",
+  siteMetadata: require("./site-metadata.json"),
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-stackbit-static-sass`,
+      options: {
+        inputFile: `${__dirname}/src/sass/main.scss`,
+        outputFile: `${__dirname}/public/assets/css/main.css`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`]
+      }
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {}
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+        menus: require("./src/data/menus.json")
+      }
+    },
+    {
+      resolve: `gatsby-plugin-webmention`,
+      options: {
+        username: "nikema.dev", // webmention.io username
+        identity: {
+          github: "prophen",
+          twitter: "dev_nikema" // no @
         },
-        {
-            resolve: `gatsby-plugin-stackbit-static-sass`,
-            options: {
-                inputFile: `${__dirname}/src/sass/main.scss`,
-                outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`]
-            }
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-                
-            }
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        }
-    ]
-};
+        mentions: true,
+        pingbacks: true,
+        domain: "nikema.dev",
+        token: process.env.WEBMENTIONS_TOKEN
+      }
+    }
+  ]
+}
